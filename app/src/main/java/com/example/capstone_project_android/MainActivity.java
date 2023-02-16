264ae8d7e46a54e4622f8b3004d4f65f5738fa59
JohnsonAudreyRalph <kalilinuxhackmaster@gmail.com>
2023-02-16 21:03:54 +0700

    Hoành thành xong yêu cầu số 1

--- app/src/main/java/com/example/capstone_project_android/MainActivity.java
+++ app/src/main/java/com/example/capstone_project_android/MainActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView Show_webView;
-    @SuppressLint("JavascriptInterface")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +26,22 @@ public class MainActivity extends AppCompatActivity {
         actionBar.hide(); //Ẩn ActionBar đi
         // Làm ẩn đi thanh trạng thái của điện thoại
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        Show_Web();
+    }
+    @SuppressLint("JavascriptInterface")
+    public void Show_Web(){
         // Trỏ tới đối tượng phía bên trên giao diện
         Show_webView = findViewById(R.id.webView);
         // Cho phép javascript được chạy
         Show_webView.getSettings().setJavaScriptEnabled(true);
         Show_webView.addJavascriptInterface(this, "runJavaScript");
-        Show_webView.loadUrl("file:///android_asset/index.html");
+        // Kiểm tra kết nối Internet của thiết bị
+        ConnectivityManager Connect =(ConnectivityManager) MainActivity.this.getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = Connect.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnectedOrConnecting()){
+            Show_webView.loadUrl("file:///android_asset/index.html");
+        }else{
+            Show_webView.loadUrl("file:///android_asset/Disconnect.html");
+        }
     }
 }
